867ee6dee711f2fa85b79835b0210c83f10bcade
saranrunran <peemai.saranporn@gmail.com>
2025-12-12 17:40:34 +0700

    lab7_2commpleted

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <cmath>
 using namespace std;
 
 //Dialog of the first example is given below.
@@ -24,15 +26,31 @@ int main() {
 
     string name;
 
-    cout << "Fahsai: Sawadee ka...Can you tell me your name?\n" << "?????: \n";
-    cin.ignore();
-    getline(cin, name);
-    cout << "\nFahsai: Wow!!! " << name << " is a really cool name.\n";
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?\n" << "?????: ";
+    getline(cin,name);
+    cout << "Fahsai: Wow!!! " << name << " is a really cool name.\n";
     
-    int id[9];
+    int id;
     cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
     cout << name << ": ";
-    cin >> id[9];
-    cout << "Fahsai: I think you may be GEAR " << 
+    cin >> id;
+    int num = id/pow(10,7); 
+    int year = num-12;  
+    cout << "Fahsai: I think you may be GEAR " << year << ". I have a free movie ticket for you.\n";
 
+    string movie;
+    cout << "Fahsai: Let's go to the cinema together!!!\n" << "Fahsai: What movie do you want to watch?\n";
+    cout << name << ": ";
+    cin.ignore();
+    getline(cin, movie);
+
+    cout << "Fahsai: So....which day are you free to go with me?\n";
+    cout << name << ": ";
+    string day;
+    getline(cin, day);
+    cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching " << movie << " with you.\n";
+    cout << name << ": ";
+    string p;
+    getline(cin,p);
+    cout << "Fahsai: 555+ see you " << day << ". Bye Bye " << '\\' << "(^ ^)/";
 }
